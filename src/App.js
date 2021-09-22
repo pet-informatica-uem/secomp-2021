8106ca48b3d46354199bef6cc2439535bad71f88
Yuri Alves <yuripiresalves@gmail.com>
2021-09-22 17:40:43 -0300

    add footer and favicon

--- src/App.js
+++ src/App.js
@@ -16,9 +16,6 @@ function App() {
             <li>
               <a href="#campeonato">CAMPEONATO</a>
             </li>
-            <li>
-              <a href="#sobre">PRÊMIOS</a>
-            </li>
           </ul>
         </nav>
       </header>
@@ -41,7 +38,7 @@ function App() {
               ficar por dentro de qualquer atualização, ou tirar suas dúvidas.
               Aproveite!
             </p>
-            <div className={styles.footer}>
+            <div className={styles.footerHero}>
               <button>INSCREVA-SE</button>
               <div className={styles.social}>
                 <ul>
@@ -163,9 +160,51 @@ function App() {
       <footer className={styles.footer}>
         <div className={styles.realizacao}>
           <h3>Realização:</h3>
+          <div className={styles.logos}>
+            <a
+              href="https://www.instagram.com/petinfouem/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="/assets/logo-pet.png" alt="Logo PET Informática" />
+            </a>
+            <a href="http://www.uem.br/" target="_blank" rel="noreferrer">
+              <img src="/assets/logo-uem.png" alt="Logo UEM" />
+            </a>
+            <a href="http://www.din.uem.br/" target="_blank" rel="noreferrer">
+              <img src="/assets/logo-din.png" alt="Logo DIN" />
+            </a>
+            <a
+              href="https://www.instagram.com/cainfouem/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="/assets/logo-cainfo.png"
+                alt="Logo Cainfo"
+                style={{ width: '70px' }}
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/caccomuem/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="/assets/logo-caccom.png"
+                alt="Logo Caccom"
+                style={{ width: '70px' }}
+              />
+            </a>
+          </div>
         </div>
         <div className={styles.patrocinio}>
-          <h3>Patrocinio:</h3>
+          <h3>Patrocínio:</h3>
+          <div className={styles.logos}>
+            <a href="/" target="_blank" rel="noreferrer">
+              <img src="/assets/logo-pet.png" alt="Logo PET Informática" />
+            </a>
+          </div>
         </div>
       </footer>
     </>

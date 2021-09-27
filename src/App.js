36abe0aef1651633a9ca3d78197860f792cd23b6
Yuri Alves <yuripiresalves@gmail.com>
2021-09-27 18:16:30 -0300

    add forms link

--- src/App.js
+++ src/App.js
@@ -53,7 +53,14 @@ function App() {
               Aproveite!
             </p>
             <div className={styles.footerHero}>
-              <button data-aos="fade-left">INSCREVA-SE</button>
+              <a
+                href="https://forms.gle/7CX5Hn9ui1sGRaD39"
+                target="_blank"
+                rel="noreferrer"
+                data-aos="fade-left"
+              >
+                INSCREVA-SE
+              </a>
               <div className={styles.social} data-aos="fade-up">
                 <ul>
                   <li>
@@ -108,69 +115,143 @@ function App() {
             class="table-fixed border-4 border-secomp"
           >
             <thead>
-            <tr class = "border-secomp border-b-2">
-              <th class = "bg-secomp bg-opacity-25 border-b-2 border-r-2 border-secomp p-4 w-3/20">Dia</th>
-              <th class = "bg-secomp bg-opacity-25 border-b-2 border-r-2 border-secomp p-4 w-1/50">Hora</th>
-              <th class = "bg-secomp bg-opacity-25 border-b-2 border-r-2 border-secomp p-4 w-3/4">Palestra</th>
+              <tr class="border-secomp border-b-2">
+                <th class="bg-secomp bg-opacity-25 border-b-2 border-r-2 border-secomp p-4 w-3/20">
+                  Dia
+                </th>
+                <th class="bg-secomp bg-opacity-25 border-b-2 border-r-2 border-secomp p-4 w-1/50">
+                  Hora
+                </th>
+                <th class="bg-secomp bg-opacity-25 border-b-2 border-r-2 border-secomp p-4 w-3/4">
+                  Palestra
+                </th>
               </tr>
             </thead>
             <tbody>
               <tr>
-              <td rowspan = "2" class = "p-4 border-b-2 border-r-2 border-secomp">04/10<br></br>(Abertura)</td>
-              <td class = "p-4 border-b-2 border-r-2 border-secomp">17:30</td>
-              <td class = "p-4 border-b-2 border-r-2 border-secomp">Como Funciona a Busca na OLX: Tecnologia e Ciência de Dados em Larga-Escala - Leonardo Wajnsztok (Senior Software Engineer na OLX)</td>
+                <td rowspan="2" class="p-4 border-b-2 border-r-2 border-secomp">
+                  04/10<br></br>(Abertura)
+                </td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">17:30</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">
+                  Como Funciona a Busca na OLX: Tecnologia e Ciência de Dados em
+                  Larga-Escala - Leonardo Wajnsztok (Senior Software Engineer na
+                  OLX)
+                </td>
               </tr>
               <tr>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">18:35</td>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">Os Desafios e Estratégias para uma Computação mais Diversa - Beatriz Tavares (Gerente de Engenharia de Software na OLX); Luana Pimentel (Software Engenieer na OLX); Taciana Melcop (Gerente de Engenharia de Software na OLX)</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">18:35</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">
+                  Os Desafios e Estratégias para uma Computação mais Diversa -
+                  Beatriz Tavares (Gerente de Engenharia de Software na OLX);
+                  Luana Pimentel (Software Engenieer na OLX); Taciana Melcop
+                  (Gerente de Engenharia de Software na OLX)
+                </td>
               </tr>
               <tr>
-                <td rowspan = "2" class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">05/10<br></br>(IA)</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">17:30</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">Reconhecimento de Padrões: Relato Acadêmico e Profissional - Arthur Rodrigues Batista (Eureka)</td>
+                <td
+                  rowspan="2"
+                  class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp"
+                >
+                  05/10<br></br>(IA)
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  17:30
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  Reconhecimento de Padrões: Relato Acadêmico e Profissional -
+                  Arthur Rodrigues Batista (Eureka)
+                </td>
               </tr>
               <tr>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">18:35</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">IA, Pesquisas Iniciais - Fernanda Ferreira (SIDI)</td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  18:35
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  IA, Pesquisas Iniciais - Fernanda Ferreira (SIDI)
+                </td>
               </tr>
               <tr>
-                <td rowspan = "2" class = "p-4 border-b-2 border-r-2 border-secomp">06/10<br></br>(Games)</td>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">17:30</td>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">Desenvolvimento de Jogos no Mercado Brasileiro - Bannaker Braulio (First Phoenix Studio)</td>
+                <td rowspan="2" class="p-4 border-b-2 border-r-2 border-secomp">
+                  06/10<br></br>(Games)
+                </td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">17:30</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">
+                  Desenvolvimento de Jogos no Mercado Brasileiro - Bannaker
+                  Braulio (First Phoenix Studio)
+                </td>
               </tr>
               <tr>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">18:35</td>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">Da Ideia ao Jogo: Como Linhas de Código e Máquinas Tiram do Papel uma Ideia Genial - Cristofer Alexandre Oswald (Fanatee)</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">18:35</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">
+                  Da Ideia ao Jogo: Como Linhas de Código e Máquinas Tiram do
+                  Papel uma Ideia Genial - Cristofer Alexandre Oswald (Fanatee)
+                </td>
               </tr>
               <tr>
-                <td rowspan = "2" class = " bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">07/10<br></br>(Segurança)</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">17:30</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">Pentest: O que é? Onde Vive? Por que toda Empresa que um? - Amanda Barbosa Sobrinho (Engenheira de Segunrança no PagSeguro)</td>
+                <td
+                  rowspan="2"
+                  class=" bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp"
+                >
+                  07/10<br></br>(Segurança)
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  17:30
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  Pentest: O que é? Onde Vive? Por que toda Empresa que um? -
+                  Amanda Barbosa Sobrinho (Engenheira de Segunrança no
+                  PagSeguro)
+                </td>
               </tr>
               <tr>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">18:35</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">Como Conteúdos de Whatsapp e Redes Sociais podem ser Usados como Prova na Justiça: a Tecnologia para Solucionar Problemas no meio Jurídico - Regina Acutu (CEO & Co-Founder da Verifact)</td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  18:35
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  Como Conteúdos de Whatsapp e Redes Sociais podem ser Usados
+                  como Prova na Justiça: a Tecnologia para Solucionar Problemas
+                  no meio Jurídico - Regina Acutu (CEO & Co-Founder da Verifact)
+                </td>
               </tr>
               <tr>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">08/10<br></br>(Mesa Redonda)</td>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">17:30</td>
-                <td class = "p-4 border-b-2 border-r-2 border-secomp">Alison Renan e Lailla Milainny Siqueira Bine</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">
+                  08/10<br></br>(Mesa Redonda)
+                </td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">17:30</td>
+                <td class="p-4 border-b-2 border-r-2 border-secomp">
+                  Alison Renan e Lailla Milainny Siqueira Bine
+                </td>
               </tr>
               <tr>
-                <td rowspan = "2" class = " bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">09/10<br></br>(Data Science e Campeonatos)</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">10:00</td>
-                <td class = "bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">Python e Machine Learning - Uma Introdução - Eduardo Maçan (Diretor de Engenharia da OLX)</td>
+                <td
+                  rowspan="2"
+                  class=" bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp"
+                >
+                  09/10<br></br>(Data Science e Campeonatos)
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  10:00
+                </td>
+                <td class="bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  Python e Machine Learning - Uma Introdução - Eduardo Maçan
+                  (Diretor de Engenharia da OLX)
+                </td>
               </tr>
               <tr>
-                <td class = " bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">14h às 19h</td>
-                <td class = " bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">Campeonatos de CS:GO, LOL e Valorant</td>
+                <td class=" bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  14h às 19h
+                </td>
+                <td class=" bg-secomp bg-opacity-25 p-4 border-b-2 border-r-2 border-secomp">
+                  Campeonatos de CS:GO, LOL e Valorant
+                </td>
               </tr>
             </tbody>
           </table>
         </section>
 
         <section id="campeonato" className={styles.sCampeonato}>
-        <h2 data-aos="fade-right">Campeonato</h2>
+          <h2 data-aos="fade-right">Campeonato</h2>
           <p data-aos="fade-down">
             O Campeonato de Jogos da Quinta Edição da SECOMP será composto por
             quatro jogos: <span>CS:GO</span>, <span>League of Legends</span>,{' '}
@@ -180,13 +261,16 @@ function App() {
             final de semana que sucede a SECOMP (9 de outubro).
           </p>
           <p data-aos="fade-down">
-            Obs: Os prêmios das equipes do campeonato será entregue somente ao primeiro lugar de cada jogo.<br></br>
-            Caso tenha um prêmio físico, ele só poderá ser resgatado quando as aulas da UEM voltarem presencialmente.
+            Obs: Os prêmios das equipes do campeonato será entregue somente ao
+            primeiro lugar de cada jogo.<br></br>
+            Caso tenha um prêmio físico, ele só poderá ser resgatado quando as
+            aulas da UEM voltarem presencialmente.
           </p>
           <h4 data-aos="fade-right">Horários</h4>
           <p data-aos="fade-down">
-            Todos os horários estão descritos no instagram da SECOMP.<br></br>Por favor, equipes que vão participar dos jogos confira sua data
-            horário correspondentes.<br></br> 
+            Todos os horários estão descritos no instagram da SECOMP.<br></br>
+            Por favor, equipes que vão participar dos jogos confira sua data
+            horário correspondentes.<br></br>
             Qualquer dúvida, contate-nos pelo Discord da SECOMP.
           </p>
 

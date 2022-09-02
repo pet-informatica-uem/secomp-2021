272646cde84351c959019a9975ee392321da5ec8
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2022-09-02 19:02:59 -0300

    Atualiza informações dos jogos - adiciona links para twitch e battlefy - muda link da twitch na pg inicial

--- src/pages/campeonato/Campeonato.jsx
+++ src/pages/campeonato/Campeonato.jsx
@@ -4,34 +4,53 @@ import Carousel from '../../components/carousel/Carousel';
 import 'aos/dist/aos.css';
 
 
-export default function Campeonato(){
-   
-    return(
-        <section id="campeonato" className={styles.sCampeonato}>
-          <h2 data-aos="fade-right">Campeonato</h2>
-          <p data-aos="fade-down">
-            O Campeonato de Jogos da Sexta Edição da SECOMP terá quatro modalidades:{' '}
-            <span>Counter-Strike: Global Offensive</span>, <span>League of Legends</span>,{' '}
-            <span>Valorant</span> e <span>Xadrez</span>. As Eliminatórias e
-            Semifinais estão previstas para acontecer de forma online, e 
-            as finais acontecerão presencialmente na UEM no
-            final de semana que sucede a SECOMP, dia 9 de outubro.
-          </p>
-          <p data-aos="fade-down">
-            Os horários ainda estão sendo elaborados. Para atualizações e novidades, siga-nos no{' '}
-            <a
-              href="https://www.instagram.com/secompuem/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Instagram
-            </a>{' '}
-            da SECOMP.<br></br>
-          </p>
+export default function Campeonato() {
 
-          <p data-aos="fade-down">Clique nas imagens para ler o regulamento.</p>
-          <Carousel/>
-        
-        </section>
-    )
+  return (
+    <section id="campeonato" className={styles.sCampeonato}>
+      <h2 data-aos="fade-right">Campeonato</h2>
+      <p data-aos="fade-down">
+        O Campeonato de Jogos da Sexta Edição da SECOMP terá quatro modalidades:{' '}
+        <span>Counter-Strike: Global Offensive</span>, <span>League of Legends</span>,{' '}
+        <span>Valorant</span> e <span>Xadrez</span>. As Eliminatórias e
+        Semifinais estão previstas para acontecer de forma online, e
+        as finais acontecerão presencialmente na UEM no
+        final de semana que sucede a SECOMP, dia 9 de outubro.
+      </p>
+      <p data-aos="fade-down">
+        As oitavas, quartas e semifinais das competições serão transmitidas no{' '}
+        <a
+          href="https://www.twitch.com/secompuem/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Twitch
+        </a>{' '}
+        oficial da SECOMP, e as partidas e o chaveamento podem ser consultados no{' '}
+        <a
+          href="https://battlefy.com/secomp-uem"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Battlefy
+        </a>{' '}
+        da SECOMP.
+      </p>
+      <p data-aos="fade-down">
+        Para saber sobre horários, atualizações e novidades, siga-nos no{' '}
+        <a
+          href="https://www.instagram.com/secompuem/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>{' '}
+        da SECOMP.<br></br>
+      </p>
+
+      <p data-aos="fade-down">Clique nas imagens para ler o regulamento.</p>
+      <Carousel />
+
+    </section>
+  )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,20 @@
 import styles from '../../Home.module.scss';
 
-export default function Home(){
-    return(
-        <section id="sobre" className={styles.sHero}>
-        <div className={styles.left}>
-          <p data-aos="fade-down">
-            A Semana da Computação, carinhosamente chamada de{' '}
-            <span>SECOMP</span>, é um evento que ocorre uma vez por ano na UEM
-            (Universidade Estadual de Maringá) e é promovido pelo Departamento
-            de Informática, o DIN. A SECOMP 2022 será a sexta edição do
-            evento e acontecerá nos{' '}
-            <span>dias 03, 04, 05, 06 e 07 de outubro</span> de 2022. A programação
-            incluirá palestras, workshops, painéis de discussões, feiras de oportunidades,
-            campeonatos de jogos e sorteios, tudo isto com o intuito de levar tecnologia
-            e informação aos participantes. Mais informações em breve!
-            {/* Convidamos você a participar do{' '}
+export default function Home() {
+  return (
+    <section id="sobre" className={styles.sHero}>
+      <div className={styles.left}>
+        <p data-aos="fade-down">
+          A Semana da Computação, carinhosamente chamada de{' '}
+          <span>SECOMP</span>, é um evento que ocorre uma vez por ano na UEM
+          (Universidade Estadual de Maringá) e é promovido pelo Departamento
+          de Informática, o DIN. A SECOMP 2022 será a sexta edição do
+          evento e acontecerá nos{' '}
+          <span>dias 03, 04, 05, 06 e 07 de outubro</span> de 2022. A programação
+          incluirá palestras, workshops, painéis de discussões, feiras de oportunidades,
+          campeonatos de jogos e sorteios, tudo isto com o intuito de levar tecnologia
+          e informação aos participantes. Mais informações em breve!
+          {/* Convidamos você a participar do{' '}
             <a
               href="https://discord.gg/6XKBDnbFsQ"
               target="_blank"
@@ -26,9 +26,9 @@ export default function Home(){
             <br></br>Todas as palestras serão transmitidas na Twitch e Youtube
             do PET-Informática, para acessá-los, clique nos ícones roxos
             abaixo. Aproveite! */}
-          </p>
-          <div className={styles.footerHero}>
-            {/* <a
+        </p>
+        <div className={styles.footerHero}>
+          {/* <a
               href="https://forms.gle/7CX5Hn9ui1sGRaD39"
               target="_blank"
               rel="noreferrer"
@@ -36,27 +36,27 @@ export default function Home(){
             >
               INSCREVA-SE
             </a> */}
-            <div className={styles.social} data-aos="fade-up">
-              <ul>
-                <li>
-                  <a
-                    href="https://www.youtube.com/c/PETInformáticaUEM"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img src="./assets/youtube-icon.svg" alt="YouTube" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://www.instagram.com/secompuem/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img src="./assets/instagram-icon.svg" alt="Instagram" />
-                  </a>
-                </li>
-                {/* <li>
+          <div className={styles.social} data-aos="fade-up">
+            <ul>
+              <li>
+                <a
+                  href="https://www.youtube.com/c/PETInformáticaUEM"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src="./assets/youtube-icon.svg" alt="YouTube" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/secompuem/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src="./assets/instagram-icon.svg" alt="Instagram" />
+                </a>
+              </li>
+              {/* <li>
                   <a
                     href="https://discord.gg/6XKBDnbFsQ"
                     target="_blank"
@@ -65,26 +65,26 @@ export default function Home(){
                     <img src="./assets/discord-icon.svg" alt="Discord" />
                   </a>
                 </li> */}
-                <li>
-                  <a
-                    href="https://www.twitch.tv/petinformaticauem"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <img src="./assets/twitch-icon.svg" alt="Twitch" />
-                  </a>
-                </li>
-              </ul>
-            </div>
+              <li>
+                <a
+                  href="https://www.twitch.tv/secompuem"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src="./assets/twitch-icon.svg" alt="Twitch" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className={styles.image} data-aos="zoom-in">
-          <img
-            src="./assets/logoteste.png"
-            alt="Logo SECOMP"
-            title="Logo SECOMP"
-          />
-        </div>
-      </section>
-    )
+      </div>
+      <div className={styles.image} data-aos="zoom-in">
+        <img
+          src="./assets/logoteste.png"
+          alt="Logo SECOMP"
+          title="Logo SECOMP"
+        />
+      </div>
+    </section>
+  )
 }
